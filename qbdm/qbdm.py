231f95b02c8287a2ea2c8a1e93c305e59c5379ad
Raghav <raghav@di.ku.dk>
2026-04-24 15:05:51 +0200

    Fix bp estimation

--- qbdm/qbdm.py
+++ qbdm/qbdm.py
@@ -206,7 +206,9 @@ def measure_complexity(model, bit_depths=[8], max_workers=8, robust=False, perce
             m_tasks = multi_tasks[bd]
             m_chunks = list(chunk_list(m_tasks, max(1, len(m_tasks) // (max_workers * 2))))
             m_results = list(executor.map(bdm_batch_worker, m_chunks))
-            total_multi[bd] = m_results#sum(sum(res) for res in m_results)
+            
+            flat = [score for chunk in m_results for score in chunk]
+            total_multi[bd] = [sum(flat[i::bd]) for i in range(bd)] # re-group by plane
 
             if tiled:
                 t_tasks = tiled_tasks[bd]
